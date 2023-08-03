f3b74660579ebd3c10375a550f75fc937d06accd
akostrik <internet124816@gmail.com>
2023-08-03 14:08:42 +0200

    -

--- srcs/main.c
+++ srcs/main.c
@@ -14,7 +14,48 @@
 
 int	g_signal = 0;
 
-int		ft_init_env(t_list **data_env, char **env);
+/**
+ * @brief 		Initilize the environment variables.
+ * 
+ * @example		They are going to be saved inside of linked list, where:
+ * 
+ * 					list->content	- full string (ex. "USER=akalimol")
+ *        			list->type      - default or not
+ * 
+ * @param data_env 	Address of env data. &data->env
+ * @param env 		environment variables passed from program
+ * 
+ * @return int 		0	- if everything ok
+ * 					-1	- if malloc failed
+ */
+int	ft_init_env(t_list **data_env, char **env)
+{
+	t_list	*token;
+	char	*str;
+	int		i;
+
+	i = 0;
+	while (env[i])
+	{
+		str = ft_strdup(env[i]);
+		if (!str)
+		{
+			ft_lstclear(data_env, &free);
+			ft_error();
+			return (-1);
+		}
+		token = ft_lstnew(str, 0);
+		if (!token)
+		{
+			free(str);
+			ft_lstclear(&token, &free);
+			return (ft_error(), -1);
+		}
+		ft_lstadd_back(data_env, token);
+		i++;
+	}
+	return (0);
+}
 
 void	ft_init_data(int argc, char **argv, char **env, t_data *data)
 {
