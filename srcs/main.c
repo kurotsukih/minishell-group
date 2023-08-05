d662aaa6ac784fb87b99bd193f34cc30362b8b1b
akostrik <internet124816@gmail.com>
2023-08-05 23:14:11 +0200

    -

--- srcs/main.c
+++ srcs/main.c
@@ -11,6 +11,9 @@
 /* ************************************************************************** */
 
 /*
+ctrl \ на моём компе не работает
+нужны ли xit code ?
+
 readline rl_clear_history, rl_on_new_line rl_replace_line rl_redisplay, add_history,
 malloc free
 printf write access open read close
