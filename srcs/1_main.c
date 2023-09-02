1d5056fa7b8d62181bc4f1aaf3fc9eeb71d45ca3
ann <internet124816@gmail.com>
2023-09-02 21:54:23 +0200

    after resert head

--- srcs/1_main.c
+++ srcs/1_main.c
@@ -6,7 +6,7 @@
 /*   By: akostrik <akostrik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 15:22:16 by akostrik          #+#    #+#             */
-/*   Updated: 2023/09/02 18:09:55 by akostrik         ###   ########.fr       */
+/*   Updated: 2023/09/02 21:53:16 by akostrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,43 @@ isatty ttyname ttyslot ioctl
 geten v 
 tcsetattr tcgetattr tgetent tgetflag tgetnum tgetstr tgoto tputs
 
-not authorised:  putenv(3), setenv(3), unsetenv(3), environ(5)
-
 SIGIN T = the user types C-c
 SIGQUI T = SIGIN T, except that it’s controlled by C-\ + produces a core dump when it terminates the process, 
 CTRL-\ causes a program to terminate and dump core
 
-cd : Quand on change de répertoire, le bash voit sa variable $PWD changer automatiquement
+cd : $PWD change
 
-si on met pas les wai tpid juste apres l'execution de la commande et qu'on les met a la fin dans une boucle il y a beaucoup de commandes avec des pipes qui ne fonctionnent plus correctement, par exemple ls|wc fait une boucle infini 
+si on met pas les wai tpid juste apres l'execution de la commande et 
+qu'on les met a la fin dans une boucle il y a beaucoup de commandes 
+avec des pipes qui ne fonctionnent plus correctement, par exemple ls|wc fait une boucle infini
 
 double quotes insdide simple ones ?
 
 " $ " is ok ?
 
-should we treat this : (?)
 a="s -la"   > l$a -> ls -la
 $a=" "      > ls$a-la$a"Makefile" -> ls -la Makefile 
 
 wc -l < infile
-tu dupliques l'entrée standard dans l'infile puis tu lances la cmd avec execve 
-
 wc -l < infile > outfile
+
+1	  les erreurs générales, comme une division par zéro
+2	  mauvaise utilisation de commandes intégrées, d'après la documentation de Bash
+126	la commande appelée ne peut s'exécuter, problème de droits ou commande non exécutable
+127	commande introuvable, problème possible avec $PATH ou erreur de frappe
+128	argument de commande invalide
+128+n	128 + le numéro du signal
+130	terminé avec Ctrl-C (130 = 128 + 2)
+255	code de sortie en dehors de la limite par ex exit -1
+
+le fils s'est terminé normalement, exit(3), _exit(2), retour de main() :
+WIFEXITED(status)   = true
+WEXITSTATUS(status) = le code de sortie du fils
+
+le fils s'est terminé à cause d'un signal:
+WIFSIGNALED(status) = vrai 
+WTERMSIG(status) = le numéro du signal
+
 */
 
 #include "headers.h"
